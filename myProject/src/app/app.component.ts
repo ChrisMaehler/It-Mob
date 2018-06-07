8b979e16cc4ac76388b46b6e26f6a6fcd8e83479
UliahTokhi <uliah.tokhi@gmx.de>
2018-06-07 17:18:42 +0200

    Registrierung neu

--- myProject/src/app/app.component.ts
+++ myProject/src/app/app.component.ts
@@ -28,15 +28,13 @@ export class MyApp {
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
       {  title: 'Test', component: TestPage},
-
-      {title: 'Registrierung', component:RegistrierungPage}
-
+      {title: 'Registrierung', component: RegistrierungPage},
       {  title: 'SignUp', component: SignUpPage}
 
     ];
 
   }
-
+  
   initializeApp() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -45,7 +43,6 @@ export class MyApp {
       this.splashScreen.hide();
     });
   }
-
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import {RegistrierungPage} from '../pages/registrierung/registrierung;
+import {RegistrierungPage} from '../pages/registrierung/registrierung';
 
 import { SignUpPage } from '../pages/sign-up/sign-up';
 
